a0327fe50eeda19c4ecfbb7ccd655eeb77b6e853
Byungchang So <sinwall@snu.ac.kr>
2024-05-16 18:58:16 +0900

    printing time; iterate over datasets

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import itertools
 from collections import defaultdict
+from datetime import datetime
 # import os
 # import sys
 # sys.path.append(os.path.abspath('../'))
@@ -29,7 +30,7 @@ def get_model(name, random_state=None, **kwargs):
     if name == 'knn':
         kwargs_default = dict(
             n_neighbors=2,
-            # weights='distance',
+            weights='distance',
         )
         kwargs_default.update(kwargs)
         kwargs = kwargs_default
@@ -68,70 +69,78 @@ def get_model(name, random_state=None, **kwargs):
 
 
 def select_geometric_params():
-    data_bundle = dict()
-    lag = (5, )
-    # scale_ws = (1e0, 2e0)
-    # scale_ds = (1e0, 2e0)
-    lags = (5, 10, 15, 20)
-    scale_ws = (5e-1, 1e0, 2e0, 4e0)
-    scale_ds = (5e-1, 1e0, 2e0, 4e0)
+    # lags = (5, )
+    # scale_ws = (1e0, )
+    # scale_ds = (1e0, )
+    lags = (5, 10, 15)
+    scale_ws = (2.5e-1, 5e-1, 1e0, 2e0, 4e0)
+    scale_ds = (2.5e-1, 5e-1, 1e0, 2e0, 4e0)
     # prepare and preprocess data
     # and save into `data_bundle`
-    data_bundle = compose(
-        load_data('NSRDB'),
-        remove_baseline(),
-        resample_ecg(fs_after=250),
-        divide_segments(seg_dur=2, fs=250),
-    )(data_bundle)
-
-    # train/test/validation split
-    y = data_bundle['seg_ids']
-    idwise_cnt_normalized = np.sum([
-        (y == s)*np.cumsum((y == s) / np.sum(y == s))
-        for s in np.unique(y)
-    ], axis=0)
-    mask_train = (idwise_cnt_normalized < 0.8) # & (idwise_cnt_normalized > 0.7)
-    mask_val = (idwise_cnt_normalized < 0.8) & (idwise_cnt_normalized > 0.7)
-
-    y_train, y_test = y[mask_train&~mask_val], y[mask_val]
-
-    model = get_model('knn', n_neighbors=5)
-    scores = defaultdict(list)
-    for lag in lags:
+    for ds_name, ds_path in zip(
+        ['MITDB', 'NSRDB', 'AFDB', 'FANTASIA'],
+        ['E:/database', 'E:/database', 'E:/database/mit-bih-atrial-fibrillation-database-1.0.0', 'E:/database']
+    ):
+        print(datetime.now().isoformat(), f'dataset={ds_name}')
+        data_bundle = dict()
         data_bundle = compose(
-            make_curves(dim=3, lag=lag, reduce=0),  # time-delay embedding
-            compress_curves(size=400),  # reduce
-            calculate_weights(scale=1e0),
+            load_data(ds_name, ds_path),
+            remove_baseline(),
+            resample_ecg(fs_after=250),
+            divide_segments(seg_dur=2, fs=250, minmax_scale=False),
         )(data_bundle)
-        for scale_w, scale_d in itertools.product(scale_ws, scale_ds):
-            for random_state in range(42, 42+5):
-                data_bundle = compose(
-                    extract_fourier(scale=scale_w, n_filters=512, random_state=random_state),
-                    extract_distance(scale=scale_d, n_filters=512, random_state=random_state),
-                )(data_bundle)
-                X_dist = data_bundle['X_dist']
-                X_fourier = data_bundle['X_fourier_w']
-                for use_weighting in [False, True]:
-                    if not use_weighting:
-                        X = X_dist
-                    else:
-                        X = np.concatenate([X_dist[..., ::2], X_fourier[..., ::2]], axis=1)
-                    X_train, X_test = X[mask_train&~mask_val], X[mask_val]
 
-                    model.fit(X_train, y_train)
-                    score = accuracy_score(y_test, model.predict(X_test))
-                    scores[(lag, scale_w, scale_d, use_weighting)].append( score )
-                    print(score, end=' ')
+        # train/test/validation split
+        y = data_bundle['seg_ids']
+        idwise_cnt_normalized = np.sum([
+            (y == s)*np.cumsum((y == s) / np.sum(y == s))
+            for s in np.unique(y)
+        ], axis=0)
+        mask_train = (idwise_cnt_normalized < 0.8) & (idwise_cnt_normalized > 0.4)
+        mask_val = (idwise_cnt_normalized < 0.8) & (idwise_cnt_normalized > 0.6)
+
+        y_train, y_test = y[mask_train&~mask_val], y[mask_val]
+
+        model = get_model('knn', n_neighbors=5)
+        scores = defaultdict(list)
+        for lag in lags:
+            print(datetime.now().isoformat(), f'lag={lag}')
+            data_bundle = compose(
+                make_curves(dim=3, lag=lag, reduce=0),  # time-delay embedding
+                compress_curves(size=400),  # reduce
+                calculate_weights(scale=1e0),
+            )(data_bundle)
+            for scale_w, scale_d in itertools.product(scale_ws, scale_ds):
+                print(datetime.now().isoformat(), f'scale_w={scale_w}, scale_d={scale_d}')
+                for random_state in range(42, 42+5):
+                    data_bundle = compose(
+                        extract_fourier(scale=scale_w, n_filters=128, random_state=random_state),
+                        extract_distance(scale=scale_d, n_filters=128, random_state=random_state),
+                    )(data_bundle)
+                    X_dist = data_bundle['X_dist']
+                    X_fourier = data_bundle['X_fourier_w']
+                    for use_weighting in [False, True]:
+                        if not use_weighting:
+                            X = X_dist
+                        else:
+                            X = np.concatenate([X_dist[..., ::2], X_fourier[..., ::2]], axis=1)
+                        X_train, X_test = X[mask_train&~mask_val], X[mask_val]
+
+                        model.fit(X_train, y_train)
+                        score = accuracy_score(y_test, model.predict(X_test))
+                        scores[(lag, scale_w, scale_d, use_weighting)].append( score )
+                        print(score, end=' ')
+                    print()
                 print()
             print()
-        print()
-    for key in scores:
-        scores[key] = np.mean(scores[key])
-    for use_weighting in (False, True):
-        print(f'If use_weighting={use_weighting}, validation result is:')
-        results = [val for key, val in scores.items() if key[-1] == use_weighting]
-        print(*results)
-        print()
+        for key in scores:
+            scores[key] = np.mean(scores[key])
+        for use_weighting in (False, True):
+            print(f'If use_weighting={use_weighting}, validation result is:')
+            results = [(key, val) for key, val in scores.items() if key[-1] == use_weighting]
+            print(*results, sep='\n')
+            print(f'optimal with {results[np.argmax([el[1] for el in results])]}')
+            print()
 
 
 def select_model_params():
@@ -139,7 +148,7 @@ def select_model_params():
     # prepare and preprocess data
     # and save into `data_bundle`
     data_bundle = compose(
-        load_data('MIT-BIH'),
+        load_data('MITDB'),
         remove_baseline(),
         resample_ecg(fs_after=250),
         divide_segments(seg_dur=2, fs=250),
@@ -221,14 +230,14 @@ def get_performances(lag=5, scale_w=2e0, scale_d=1e0):
     # prepare and preprocess data
     # and save into `data_bundle`
     data_bundle = compose(
-        load_data('MIT-BIH'),
+        load_data('MITDB', 'E:/database'),
         remove_baseline(),
         resample_ecg(fs_after=250),
-        divide_segments(seg_dur=2, fs=250),
+        divide_segments(seg_dur=2, fs=250, minmax_scale=True),
         make_curves(dim=3, lag=lag, reduce=0),  # time-delay embedding
         compress_curves(size=250),  # reduce
         calculate_weights(scale=1e0),
-        calculate_max_dispers(scale=1e0),
+        # calculate_max_dispers(scale=1e0),
     )(data_bundle)
 
     # train/test/validation split
@@ -239,7 +248,7 @@ def get_performances(lag=5, scale_w=2e0, scale_d=1e0):
     ], axis=0)
     mask_train = (idwise_cnt_normalized < 0.8) # & (idwise_cnt_normalized > 0.7)
     mask_test = idwise_cnt_normalized > 0.8
-    mask_val = (idwise_cnt_normalized < 0.8) & (idwise_cnt_normalized > 0.7)
+    # mask_val = (idwise_cnt_normalized < 0.8) & (idwise_cnt_normalized > 0.7)
 
     y_train, y_test = y[mask_train], y[mask_test]
     scores = defaultdict(list)
@@ -247,21 +256,38 @@ def get_performances(lag=5, scale_w=2e0, scale_d=1e0):
     # for model_name in ['knn', 'hgbt', 'lr']:
         print(model_name)
         for random_state in range(42, 42+10):
-            data_bundle = compose(
-                extract_fourier(scale=scale_w, n_filters=512, random_state=random_state),
-                extract_fourier(scale=scale_w, n_filters=512, weight_type='m', random_state=random_state),
-                extract_distance(scale=scale_d, n_filters=512, random_state=random_state),
-            )(data_bundle)
-            X_dist = data_bundle['X_dist']
-            X_fourier_w = data_bundle['X_fourier_w']
-            X_fourier_m = data_bundle['X_fourier_m']
+            # data_bundle = compose(
+            #     extract_fourier(scale=scale_w, n_filters=512, random_state=random_state),
+            #     extract_fourier(scale=scale_w, n_filters=512, weight_type='m', random_state=random_state),
+            #     extract_distance(scale=scale_d, n_filters=512, random_state=random_state),
+            # )(data_bundle)
+            # X_dist = data_bundle['X_dist']
+            # X_fourier_w = data_bundle['X_fourier_w']
+            # X_fourier_m = data_bundle['X_fourier_m']
 
-            for use_weighting in [False, 'w', 'm']:
+            for use_weighting in [False, 'w']:
+            # for use_weighting in [False, 'w', 'm']:
                 if not use_weighting:
+                    data_bundle = compose(
+                        extract_distance(scale=2e0, n_filters=512, random_state=random_state)
+                    )(data_bundle)
+                    X_dist = data_bundle['X_dist']
                     X = X_dist
                 elif use_weighting == 'w':
+                    data_bundle = compose(
+                        extract_fourier(scale=1e0, n_filters=512, random_state=random_state),
+                        extract_distance(scale=1e0, n_filters=512, random_state=random_state),
+                    )(data_bundle)
+                    X_dist = data_bundle['X_dist']
+                    X_fourier_w = data_bundle['X_fourier_w']
                     X = np.concatenate([X_dist[...,::-2], X_fourier_w[..., ::-2]], axis=1)
                 elif use_weighting == 'm':
+                    data_bundle = compose(
+                        extract_fourier(scale=1e0, n_filters=512, weight_type='m', random_state=random_state),
+                        extract_distance(scale=1e0, n_filters=512, random_state=random_state),
+                    )(data_bundle)
+                    X_dist = data_bundle['X_dist']
+                    X_fourier_m = data_bundle['X_fourier_m']
                     X = np.concatenate([X_dist[...,::-2], X_fourier_m[..., ::-2]], axis=1)
                 X_train, X_test = X[mask_train], X[mask_test]
 
@@ -271,9 +297,9 @@ def get_performances(lag=5, scale_w=2e0, scale_d=1e0):
                 scores[(model_name, use_weighting)].append( score )
                 print(score, end=' ')
             print()
-        print(f'result stats (without w ): mean {np.mean(scores[(model_name, False)])}, std:{np.std(scores[(model_name, False)])}')
-        print(f'result stats (with    w ): mean {np.mean(scores[(model_name, "w")])}, std:{np.std(scores[(model_name, "w")])}')
-        print(f'result stats (with    w+): mean {np.mean(scores[(model_name, "m")])}, std:{np.std(scores[(model_name, "m")])}')
+        print(f'result stats (wout w ): mean {np.mean(scores[(model_name, False)])}, std:{np.std(scores[(model_name, False)])}')
+        print(f'result stats (with w ): mean {np.mean(scores[(model_name, "w")])}, std:{np.std(scores[(model_name, "w")])}')
+        # print(f'result stats (with w+): mean {np.mean(scores[(model_name, "m")])}, std:{np.std(scores[(model_name, "m")])}')
         print()
 
 if __name__ == '__main__':
